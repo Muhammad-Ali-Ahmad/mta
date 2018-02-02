a997b83eefc899c1b0c11b09a156843d1940a426
muhammad.ahmad <muhammad.ahmad@digital.hmrc.gov.uk>
2018-02-02 18:38:46 +0000

    update

--- js/site.js
+++ js/site.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 	$('a.next').click(function(){
-		$('.video-list').css('transform', 'translateX(-200px)');
+		$('.video-list ul').css('transform', 'translate3d(-1620px, 0px, 0px)');
+	});
+	$('a.prev').click(function(){
+		$('.video-list ul').css('transform', '');
 	});
 });
