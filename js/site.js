0efaf4381d259253dad92ad14e6b680d55d4e94d
muhammad.ahmad <muhammad.ahmad@digital.hmrc.gov.uk>
2018-02-27 18:30:06 +0000

    scroll update

--- js/site.js
+++ js/site.js
@@ -1,8 +1,27 @@
 $(document).ready(function(){
-	$('a.next').click(function(){
-		$('.video-list ul').css('transform', 'translate3d(-1620px, 0px, 0px)');
+
+
+	$(window).resize(function() {
+		videoScroll();
 	});
-	$('a.prev').click(function(){
-		$('.video-list ul').css('transform', '');
-	});
+
+	videoScroll();
 });
+
+function videoScroll() {
+	var wrapperWidth = $('.video-list-wrapper').width();
+	var defaultWrapperWidth = wrapperWidth;
+	var prev
+
+	$('a.next').click(function(){	
+		prev = (wrapperWidth-75)
+		$('.video-list ul').css('transform', 'translate3d(-' + (wrapperWidth-75) + 'px, 0px, 0px)');
+		wrapperWidth += (defaultWrapperWidth - 75)
+	});
+
+	$('a.prev').click(function(){
+		wrapperWidth = (prev + 75)
+		prev = (prev - defaultWrapperWidth + 75)
+		$('.video-list ul').css('transform', 'translate3d(-'+ prev + 'px, 0px, 0px)');
+	});
+}
