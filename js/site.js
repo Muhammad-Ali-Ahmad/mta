6a3bb7afdc7b00c3c3a82f7c3e2f3b365fc6aa74
muhammad.ahmad <muhammad.ahmad@digital.hmrc.gov.uk>
2018-03-03 23:54:14 +0000

    js update

--- js/site.js
+++ js/site.js
@@ -11,33 +11,95 @@ $(document).ready(function(){
 	console.log($('.video-list li').length);
 });
 
+// function videoScroll() {
+// 	var wrapperWidth = $('.video-list-wrapper').width();
+// 	var defaultWrapperWidth = wrapperWidth;
+// 	var prev
+
+// 	$('a.next').click(function(){	
+// 		prev = (wrapperWidth-75)
+// 		$('.video-list-control a.prev').css('display', 'block');
+// 		// if (wrapperWidth == 615) {
+// 		// 	$('.video-list-control a.prev').css('display', 'none');
+// 		// }
+// 		$('.video-list ul').css('transform', 'translate3d(-' + (wrapperWidth-75) + 'px, 0px, 0px)');
+// 		wrapperWidth += (defaultWrapperWidth - 75)
+// 	});
+
+// 	$('a.prev').click(function(){
+// 		if (defaultWrapperWidth == 345 && wrapperWidth == 615 ||
+// 			defaultWrapperWidth == 615 && wrapperWidth == 1155 ||
+// 			defaultWrapperWidth == 885 && wrapperWidth == 1695 ||
+// 			defaultWrapperWidth == 1155 && wrapperWidth == 2235 ||
+// 			defaultWrapperWidth == 1425 && wrapperWidth == 2775) {
+// 			$('.video-list-control a.prev').css('display', 'none');
+// 		} else {
+// 			$('.video-list-control a.prev').css('display', 'block');
+// 		}
+// 		wrapperWidth = (prev + 75)
+// 		prev = (prev - defaultWrapperWidth + 75)
+// 		$('.video-list ul').css('transform', 'translate3d(-'+ prev + 'px, 0px, 0px)');
+// 	});
+// }
+
+
 function videoScroll() {
-	var wrapperWidth = $('.video-list-wrapper').width();
-	var defaultWrapperWidth = wrapperWidth;
-	var prev
+	var wrapperWidth = $('.video-list-wrapper').width(),
+	    defaultWrapperWidth = wrapperWidth,
+	    prev,
+	    i = 1,
+	    itemLength,
+	    numberOfItems,
+	    numberOfItemsOnScreen,
+	    remainder,
+	    numberOfTimesToScroll;
 
 	$('a.next').click(function(){	
-		prev = (wrapperWidth-75)
+		itemLength = $('.video-list li').width(),
+		numberOfItems = $('.video-list li').length,
+		numberOfItemsOnScreen = (Math.floor(defaultWrapperWidth/itemLength)),
+		remainder = numberOfItems % numberOfItemsOnScreen,
+		numberOfTimesToScroll = Math.floor(numberOfItems / numberOfItemsOnScreen);
+
+		if(i < numberOfTimesToScroll){
+			shift = (((itemLength*numberOfItemsOnScreen) + (numberOfItemsOnScreen * 20)) * i)
+			i++
+		} else {
+			if(remainder > 0) {
+				shift = (((itemLength*numberOfItemsOnScreen) + (numberOfItemsOnScreen * 20)) * (i * (remainder/numberOfItemsOnScreen)))
+				remainder = 0;
+			}
+		}
+
+		if (i == numberOfTimesToScroll && remainder == 0) {
+			$('.video-list-control a.next').css('display', 'none');
+		}
 		$('.video-list-control a.prev').css('display', 'block');
-		// if (wrapperWidth == 615) {
-		// 	$('.video-list-control a.prev').css('display', 'none');
-		// }
-		$('.video-list ul').css('transform', 'translate3d(-' + (wrapperWidth-75) + 'px, 0px, 0px)');
-		wrapperWidth += (defaultWrapperWidth - 75)
+
+		// prev = (wrapperWidth-75)
+
+		$('.video-list ul').css('transform', 'translate3d(-' + shift + 'px, 0px, 0px)');
+		// wrapperWidth += (defaultWrapperWidth - 75)
 	});
 
 	$('a.prev').click(function(){
-		if (defaultWrapperWidth == 345 && wrapperWidth == 615 ||
-			defaultWrapperWidth == 615 && wrapperWidth == 1155 ||
-			defaultWrapperWidth == 885 && wrapperWidth == 1695 ||
-			defaultWrapperWidth == 1155 && wrapperWidth == 2235 ||
-			defaultWrapperWidth == 1425 && wrapperWidth == 2775) {
-			$('.video-list-control a.prev').css('display', 'none');
+		
+		remainder = numberOfItems % numberOfItemsOnScreen;
+
+console.log(i)
+
+		if(i >= numberOfTimesToScroll){
+			i--
+			shift -= (((itemLength*numberOfItemsOnScreen) + (numberOfItemsOnScreen * 20)) * i)
 		} else {
-			$('.video-list-control a.prev').css('display', 'block');
+			if(remainder > 0) {
+				remainder--
+				shift -= (((itemLength*numberOfItemsOnScreen) + (numberOfItemsOnScreen * 20)) * (i * (remainder/numberOfItemsOnScreen)))
+			}
 		}
-		wrapperWidth = (prev + 75)
-		prev = (prev - defaultWrapperWidth + 75)
-		$('.video-list ul').css('transform', 'translate3d(-'+ prev + 'px, 0px, 0px)');
+
+		$('.video-list-control a.next').css('display', 'block');
+
+		$('.video-list ul').css('transform', 'translate3d(-'+ shift + 'px, 0px, 0px)');
 	});
 }
