841ec17cfbfeeca2fa1e5d9c76165059ecd7d677
muhammad.ahmad <muhammad.ahmad@digital.hmrc.gov.uk>
2018-03-08 18:19:19 +0000

    update

--- js/site.js
+++ js/site.js
@@ -64,3 +64,36 @@ function next(display) {
 function prev(display) {
 	$('.video-list-control a.prev').css('display', display);
 }
+
+
+
+// function videoScroll() {
+// 	var wrapperWidth = $('.video-list-wrapper').width();
+// 	var defaultWrapperWidth = wrapperWidth;
+// 	var prev
+
+// 	$('a.next').click(function(){	
+// 		prev = (wrapperWidth-75)
+// 		$('.video-list-control a.prev').css('display', 'block');
+// 		// if (wrapperWidth == 615) {
+// 		// 	$('.video-list-control a.prev').css('display', 'none');
+// 		// }
+// 		$('.video-list ul').css('transform', 'translate3d(-' + (wrapperWidth-75) + 'px, 0px, 0px)');
+// 		wrapperWidth += (defaultWrapperWidth - 75)
+// 	});
+
+// 	$('a.prev').click(function(){
+// 		if (defaultWrapperWidth == 345 && wrapperWidth == 615 ||
+// 			defaultWrapperWidth == 615 && wrapperWidth == 1155 ||
+// 			defaultWrapperWidth == 885 && wrapperWidth == 1695 ||
+// 			defaultWrapperWidth == 1155 && wrapperWidth == 2235 ||
+// 			defaultWrapperWidth == 1425 && wrapperWidth == 2775) {
+// 			$('.video-list-control a.prev').css('display', 'none');
+// 		} else {
+// 			$('.video-list-control a.prev').css('display', 'block');
+// 		}
+// 		wrapperWidth = (prev + 75)
+// 		prev = (prev - defaultWrapperWidth + 75)
+// 		$('.video-list ul').css('transform', 'translate3d(-'+ prev + 'px, 0px, 0px)');
+// 	});
+// }
