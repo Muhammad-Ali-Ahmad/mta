03c6f53850ece33f0cbb61c9d00311fd0e4921a7
Muhammad-Ali-Ahmad <muhammad.ahmad@digital.hmrc.gov.uk>
2018-03-25 11:44:04 +0100

    js clean up

--- js/time.js
+++ js/time.js
@@ -1,76 +1,75 @@
-var s = "23:00";
-var e = "02:15";
+var s = "11:00";
+var e = "12:00";
 
+
+
+function getProgrammeLength(start, end) {
+    "use strict";
+    var hour = end[0] - start[0];
+    var minutes = end[1] - start[1];
+
+    if (hour < 0) {
+        hour += 24;
+    }
+    if (minutes < 0) {
+        minutes += 60;
+        hour -= 1;
+    }
+    return {hour: hour, minutes: minutes, totalMinutes: minutes + (hour * 60)};
+}
+
+
+function getProgrammeEnd(now, start, end, hour, minutes) {
+    "use strict";
+    var endingHour;
+    var endingMinutes;
+    var day = now.getDate();
+    var month = now.getMonth();
+    var year = now.getFullYear();
+    var hourNow = now.getHours();
+
+    endingMinutes = Number(start[1]) + minutes;
+    endingHour = Number(start[0]) + hour;
+
+    if (endingMinutes > 60) {
+        endingMinutes -= 60;
+        endingHour += 1;
+    }
+
+    if (endingHour >= 24) {
+        endingHour -= 24;
+        var midnight;
+        if (Number(start[0]) >= 21 && Number(end[0] < 3) && hourNow >= 21) {
+            midnight = new Date(month + 1 + " " + (day + 1) + ", " + year + " 00:00:00");
+        } else {
+            midnight = new Date(month + 1 + " " + day + ", " + year + " 00:00:00");
+        }
+
+        if (now.getTime() < midnight.getTime()) {
+            day += 1;
+        }
+    }
+
+    return new Date(year, month, day, endingHour, endingMinutes, 0);
+}
 
 function programmeBarPrecentage(start, end) {
     "use strict";
     start = start.split(":");
     end = end.split(":");
-    var now = new Date("March 25, 2018 00:15:00"),
-        programmeLength = getProgrammeLength(start, end),
-        programmeEnd = getProgrammeEnd(now, start, end, programmeLength["hour"], programmeLength["minutes"]),
-        minutesRemaining = (programmeEnd.getTime() - now.getTime()) / 60000,
-        precentage = Math.floor(100 - ((minutesRemaining / programmeLength["totalMinutes"]) * 100));
+    var now = new Date();
+    var programmeLength = getProgrammeLength(start, end);
+    var programmeEnd = getProgrammeEnd(now, start, end, programmeLength.hour, programmeLength.minutes);
+    var minutesRemaining = (programmeEnd.getTime() - now.getTime()) / 60000;
+    var precentage = Math.floor(100 - ((minutesRemaining / programmeLength.totalMinutes) * 100));
 
-    if(precentage < 0) {
-        return 0
+    if (precentage < 0) {
+        return 0;
     } else if (precentage > 100) {
-        return 100
+        return 100;
     } else {
-        return precentage
+        return precentage;
     }
-}
-
-
-function getProgrammeLength (start, end) {
-    var hour = end[0] - start[0],
-        minutes = end[1] - start[1];
-
-        if (hour < 0) {
-            hour += 24
-        }
-        if (minutes < 0) {
-            minutes += 60;
-            hour--;
-        }
-        return {hour: hour, 
-                minutes: minutes,
-                totalMinutes: minutes+(hour*60)}
-}
-
-
-function getProgrammeEnd(now, start, end, hour, minutes) {
-        var endingHour,
-        endingMinutes,
-        endingDate,
-        day = now.getDate(),
-        month = now.getMonth(),
-        year = now.getFullYear(),
-        hourNow = now.getHours(),
-        midnight;
-
-        if(Number(start[0]) > 21 && Number(end[0] < 3) && hourNow >= 21) {
-            midnight = new Date(month+1 + " " + (day+1) +", " + year + " 00:00:00");
-        } else {
-            midnight = new Date(month+1 + " " + day +", " + year + " 00:00:00");
-        }
-
-        endingMinutes  = Number(start[1]) + minutes;
-        endingHour = Number(start[0]) + hour;
-
-        if (endingMinutes > 60) {
-            endingMinutes -= 60;
-            endingHour++
-        }
-
-        if(endingHour >= 24) {
-            endingHour -= 24;
-            if(now.getTime() < midnight.getTime()) {
-                day++;
-            }
-        }
-
-        return new Date(year, month, day, endingHour, endingMinutes, 0);
 }
 // custom time
 //    var now = new Date((1516109400*1000));
