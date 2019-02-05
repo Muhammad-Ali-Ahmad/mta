b46777c4fc7ba754453c5896bc79a3c1d036da33
muhammad <17907161+Muhammad-Ali-Ahmad@users.noreply.github.com>
2019-02-05 23:56:53 +0000

    photo swipe test

--- js/site.js
+++ js/site.js
@@ -1,7 +1,36 @@
-$(document).ready(function() {
-    console.log("working");
-});
 
+$(document).ready(function() {
+	var pswpElement = document.querySelectorAll('.pswp')[0];
+
+	// build items array
+	var items = [
+		{
+			src: 'https://placekitten.com/600/400',
+			w: 600,
+			h: 400
+		},
+		{
+			src: 'https://placekitten.com/1200/900',
+			w: 1200,
+			h: 900
+		}
+	];
+
+	// define options (if needed)
+	var options = {
+		// optionName: 'option value'
+		// for example:
+		index: 0 // start at first slide
+	};
+
+
+	$('#showpic').click(function() {
+		// Initializes and opens PhotoSwipe
+		var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+	
+		gallery.init();
+	});
+});
 // var slideIndex = 1;
 // showSlides(slideIndex);
 
